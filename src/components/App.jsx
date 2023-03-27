5f7bb7ffe05db346efe130704f68d8f01f4cfb84
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-27 04:51:09 +0300

    changed file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import { Route,Routes } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import '../stylesheets/App.css';
 
 function App() {
   const [count, setCount] = useState(0)
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import App from './App'
-import './index.css'
-import { UserDataProvider } from './UserDataProvider';
+import App from './components/App'
+import './stylesheets/index.css'
+import { UserDataProvider } from './components/UserDataProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
